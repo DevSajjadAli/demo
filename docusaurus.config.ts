207df561ed408fa80d139488f7332752dd33434c
Habib Ullah <jacksbrown443@gmail.com>
2025-06-25 11:19:17 +0500

    upd

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -16,10 +16,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://omegaark.github.io',
+  url: 'https://docs.hopevalley.cloud',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/demo/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
